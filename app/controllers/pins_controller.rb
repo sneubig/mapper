83ef2982204b3a927b7a07d9379cd91328a54e1e
sneubig <neubig.stephen@gmail.com>
2014-09-29 16:58:10 -0400

    loaded map to view

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,10 +1,27 @@
 class PinsController < ApplicationController
 
 	def index
+		@pins = Pin.all
 
-	end
+	    respond_to do |format|
+	      format.html
+	      format.json { render json: @todos }
+	    end
+    end
 
 	def create
+    	@pin = Pin.new(pin_params)
 
-	end
+	    if @pin.save
+	      respond_to do |format|
+	        format.html
+	        format.json { render json: @todo }
+	      end
+	    else
+	      respond_to do |format|
+	        format.html { render :new }
+	        format.json { render status: 400, nothing: true }
+	      end
+	    end
+  	end
 end
